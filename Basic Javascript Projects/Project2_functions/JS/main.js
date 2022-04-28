ff56273bf7f8ab2d9aacbd6e5637f34f2b222b8f
Danfran165 <danfran165@yahoo.com>
2022-04-28 15:12:51 -0500

    js

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function changecolor (newColor) {
+    var elem = document.getElementById ('para');
+    elem.style.color =newColor;
+}
+function myFunction(button) {
+    var sentence = "I am learning";
+    sentence += " a lot from this book";
+    document.getElementById("Concatenate") .innerHTML = sentence;
+//here im using a funtion that uses the += operator
+}
---
+++ Basic Javascript Projects/main.js
@@ -0,0 +1,15 @@
+window.alert ("Hello World!");
+document.write ("Sierra told the dog, \"don't do that, bear! or ill tell mom! bear looked at her and huffed.");
+document.write ("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind. \""
++ "-Dr. Suess") ; 
+var D = "Hi there";
+document.write (D)
+fontcolor("blue");
+var family = "the frangiones", Mom = "Danielle" , Mommy = "Sierra" , Dog = "Bear";
+document.write(Mommy);
+
+document.write (3+3);//my code is adding three plus three
+function changecolor(blue) {
+    document.bgcolor=red;
+}
