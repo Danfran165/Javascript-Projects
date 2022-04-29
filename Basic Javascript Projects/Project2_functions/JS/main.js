5526af7c3dd8b40eaa6f8ae0d9b1b891a792b462
Danfran165 <danfran165@yahoo.com>
2022-04-29 11:12:01 -0500

    resubmit

--- Basic Javascript Projects/Project2_functions/JS/main.js
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -1,8 +1,8 @@
-function changecolor (newColor) {
+function changeColor (newColor) {
     var elem = document.getElementById ('para');
     elem.style.color =newColor;
 }
-function myFunction(button) {
+function myFunction(para) {
     var sentence = "I am learning";
     sentence += " a lot from this book";
     document.getElementById("Concatenate") .innerHTML = sentence;
