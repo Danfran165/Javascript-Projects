2c11f8ebfdf6d25f4af34bc65edbc97f719ac059
Danfran165 <danfran165@yahoo.com>
2022-04-30 15:26:55 -0500

    project 4

--- Basic Javascript Projects/Project4_Dictionaries/main.js
+++ Basic Javascript Projects/Project4_Dictionaries/main.js
@@ -7,6 +7,6 @@ function my_Dictionary() {
         Sound:"Meow!"
 
     };
-    delete Cat.Age;
+    delete Cat.Age; //this is the delete funtion which will make the browser come up undefined
     document.getElementById("Dictionary").innerHTML = Cat.Age;
 }
