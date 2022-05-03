1490ec494b331cc7b3392cae79d9b94269cfc5c7
Danfran165 <danfran165@yahoo.com>
2022-05-03 14:59:59 -0500

    submit

---
+++ Basic Javascript Projects/Project5_comparisons_type_coercion/main.js
@@ -0,0 +1,28 @@
+
+document.write("15"+9);
+function my_Function() {
+    document.getElementById("Test") .innerHTML = 0/0;
+
+}
+document.write(2E310);
+document.write(-3E310);
+document.write(5>6);
+document.write(3<4);
+console.log(4+4);
+console.log(4>5);
+document.write(3==3);
+document.write(2==5);
+X = 5;
+Y = 5;
+document.write(X===Y);
+X = 56;
+Y = 87;
+document.write(X===Y);
+document.write(4>3 && 9>5);
+document.write(7>9 && 18>8);
+document.write(6>7 || 9>6);
+document.write(8>67 || 30>40);
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = ! (5 >10);
+}
