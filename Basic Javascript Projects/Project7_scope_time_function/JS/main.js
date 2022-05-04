fe3002f2c650ac53940961aae8ba78c8eeb56072
Danfran165 <danfran165@yahoo.com>
2022-05-04 15:47:37 -0500

    project8

--- Basic Javascript Projects/Project7_scope_time_function/JS/main.js
+++ Basic Javascript Projects/Project7_scope_time_function/JS/main.js
@@ -9,3 +9,11 @@ function get_Date() {
     }
 }
 
+function full_sentence() {
+    var part_1 = "I have ";
+    var part_2 = "made this ";
+    var part_3 = "into a complete ";
+    var part_4 = "sentence. ";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
